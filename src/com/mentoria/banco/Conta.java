8b58c964ee8bbd77f2ed23ad86fddff9ccecb0f9
silasbsi <silasbsi@gmail.com>
2015-04-15 13:34:56 -0300

    Rejected commit: Default (2)

--- src/com/mentoria/banco/Conta.java
+++ src/com/mentoria/banco/Conta.java
@@ -2,7 +2,7 @@ package com.mentoria.banco;
 
 public class Conta {
 	private String dono = "Silas";
-	private String cpf = "068.529.104-90";
+	private String cpf = "000.000.000-00";
 	private double saldo = 1000.0;
 
 	//Dono
@@ -48,7 +48,55 @@ public class Conta {
 			return false;
 		}
 	}
-	
-	
-	
+
+    public void consultaNotas(double valor) {
+        int resto = 0, quantidadeCem = 0, quantidadeCinquenta = 0, quantidadeVinte = 0,
+                quantidadeDez = 0, quantidadeCinco = 0, quantidadeDois = 0, quantidadeUm = 0;
+
+        if (valor >= 100) {
+            quantidadeCem = (int)valor / 100;
+            resto = (int)valor % 100;
+            valor = resto;
+        }
+        if (valor >= 50) {
+            quantidadeCinquenta = (int)valor / 50;
+            resto = (int)valor % 50;
+            valor = resto;
+        }
+        if (valor >= 20) {
+            quantidadeVinte = (int)valor / 20;
+            resto = (int)valor % 20;
+            valor = resto;
+        }
+        if (valor >= 10) {
+            quantidadeDez = (int)valor / 10;
+            resto = (int)valor % 10;
+            valor = resto;
+        }
+        if (valor >= 5) {
+            quantidadeCinco = (int)valor / 5;
+            resto = (int)valor % 5;
+            valor = resto;
+        }
+        if (valor >= 2){
+            quantidadeDois = (int)valor / 2;
+            resto = (int)valor % 2;
+            valor = resto;
+        }
+        if (valor >= 1) {
+            quantidadeUm = (int)valor / 1;
+            resto = (int)valor % 2;
+            valor = resto;
+        }
+
+        System.out.println(quantidadeCem + " notas de 100");
+        System.out.println(quantidadeCinquenta + " notas de 50");
+        System.out.println(quantidadeVinte + " notas de 20");
+        System.out.println(quantidadeDez + " notas de 10");
+        System.out.println(quantidadeCinco + " notas de 5");
+        System.out.println(quantidadeDois + " notas de 2");
+        System.out.println(quantidadeUm + " moeda de 1");
+
+    }
+
 }
--- src/com/mentoria/banco/Main.java
+++ src/com/mentoria/banco/Main.java
@@ -1,38 +1,41 @@
 package com.mentoria.banco;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {		
-		int op;	
-		
-		Conta conta = new Conta();
-		
+		int op;
+
+        Conta conta = new Conta();
+
+
 		do {
 			System.out.println("\nAjuda Banqueiro");
-			System.out.println("1 � Sacar");
-			System.out.println("2 � Depositar");
-			System.out.println("3 � Transfer�ncia");
-			System.out.println("4 � Consultar Notas");
-			System.out.println("5 � Verificar Caixa");
-			System.out.println("6 � Sair\n");
-			
+			System.out.println("1 - Sacar");
+			System.out.println("2 - Depositar");
+			System.out.println("3 - Transferencia");
+			System.out.println("4 - Consultar Notas");
+			System.out.println("5 - Verificar Caixa");
+			System.out.println("6 - Sair\n");
+
 			Scanner scanner = new Scanner(System.in);
-			System.out.print("Op��o: ");
+			System.out.print("Opcao: ");
 			op = scanner.nextInt();
-			
+
 			if (op == 1) {
 				System.out.print("Valor de saque: ");
 				double valor = scanner.nextDouble();
 				boolean saca = conta.saca(valor);
-			
+
 				if (saca) {
 					System.out.println("Saque efetuado com sucesso.");
-				} else { 
-					System.out.println("N�o h� saldo suficiente.");
-				}	
+				} else {
+					System.out.println("Nao ha saldo suficiente.");
+				}
 			} else if (op == 2) {
 				System.out.print("Valor de deposito: ");
 				double valor = scanner.nextDouble();
@@ -41,12 +44,16 @@ public class Main {
 				boolean transf = conta.transfere(conta, 100);
 				
 				if (transf) {
-					System.out.println("Transfer�ncia feita com sucesso.\n");
+					System.out.println("Transferencia feita com sucesso.\n");
 				} else { 
-					System.out.println("O saldo n�o � suficiente para fazer essa tranfer�ncia.\n");
+					System.out.println("O saldo nao e suficiente para fazer essa tranferencia.\n");
 				}
 			} else if (op == 4) {
-				System.out.println("Em constru��o...");
+                System.out.print("Valor: ");
+                double valor = scanner.nextDouble();
+                conta.consultaNotas(valor);
+
+
 			} else if (op == 5){
 				System.out.println("\n=====================");
 				System.out.println("Dono: " + conta.getDono());
@@ -60,8 +67,8 @@ public class Main {
 			
 			
 		} while (op != 6);
-		
-		
+
+
 		
 	}
 
